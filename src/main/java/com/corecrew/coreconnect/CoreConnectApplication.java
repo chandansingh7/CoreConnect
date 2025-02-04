80d7a86a0887ed868547f1ac9d2fea60ea969ac1
chandan <chandan.singh558@gmail.com>
2025-02-04 15:24:04 -0600

    Initial commit

---
+++ src/main/java/com/corecrew/coreconnect/CoreConnectApplication.java
@@ -0,0 +1,13 @@
+package com.corecrew.coreconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/corecrew/coreconnect/CoreConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corecrew.coreconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
